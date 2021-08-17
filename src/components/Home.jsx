a25a1a0e1609201ff0e5a340d78ae93d4869ff5e
Siddharth <siddharth201820@gmail.com>
2021-08-17 18:48:05 +0530

    grid added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,54 @@
 import React, { useEffect } from "react";
+
+// Redux
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../actions/index";
 
+// Material UI
+import { makeStyles, Grid, Paper } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexGrow: 1,
+    justifyContent: "center",
+    width: "100vw",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+  },
+}));
+
 const Home = () => {
-  const { products } = useSelector((state) => state.products);
+  const products = useSelector((state) => state.products);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getProducts());
-  }, [dispatch]);
-  console.log(products);
+  }, []);
+
+  const classes = useStyles();
 
   return (
     <div>
       <h1>Home Page</h1>
-      {/* {products.map((product) => (
+      <div className={classes.root}>
+        <Grid container spacing={6}>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+        </Grid>
+        {/* {products.map((product) => (
         <h1>{product.id}</h1>
       ))} */}
+      </div>
     </div>
   );
 };
