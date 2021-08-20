2de159d3ae72650e3a605860c18c3180aa9f3f03
Siddharth <siddharth201820@gmail.com>
2021-08-20 13:00:42 +0530

    placeholder for bill details in checkout page

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import BillDetails from "./BillDetails";
+import BillDetailsPlaceholder from "./BillDetailsPlaceholder";
 import CheckoutItem from "./CheckoutItem";
 import CheckoutItemPlaceholder from "./CheckoutItemPlaceholder";
 
@@ -25,7 +26,11 @@ const Checkout = () => {
             ))}
       </div>
       {/* Order Details */}
-      <BillDetails cart={cart} />
+      {products.length ? (
+        <BillDetails cart={cart} />
+      ) : (
+        <BillDetailsPlaceholder />
+      )}
     </div>
   );
 };
